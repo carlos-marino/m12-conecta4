5221e374a92a55ea3bee529b6bf3cdec6e4b2261
carlos-marino <carlos_marinosenra@iescarlesvallbona.cat>
2024-10-09 20:26:29 +0200

    Segon commit, acabar el taulell i omplir-lo amb les fitxes.

--- src/projectofol/main.java
+++ src/projectofol/main.java
@@ -4,7 +4,7 @@
  */
 package projectofol;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *
@@ -17,41 +17,100 @@ public class main {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Ingrese el número de files: ");
-        int filas = scanner.nextInt();
-        
+        int files = scanner.nextInt();
+
         System.out.print("Ingrese el número de columnes: ");
-        int columnas = scanner.nextInt();
-        
-        char[][] tablero = new char[filas][columnas];
-        
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                tablero[i][j] = '-';
+        int columnes = scanner.nextInt();
+
+        char[][] taulell = new char[files][columnes];
+
+        inicialitzarTaulell(taulell);
+        imprimirTaulell(taulell);
+
+        boolean jocEnMarxa = true;
+        char jugadorActual = 'X';
+
+        while (jocEnMarxa) {
+            System.out.println("Torn del jugador " + jugadorActual);
+            int columna;
+
+            do {
+                System.out.print("Introdueix una columna (1-" + columnes + "): ");
+                columna = scanner.nextInt() - 1;
+            } while (!columnaValida(taulell, columna));
+
+            colocarFitxa(taulell, columna, jugadorActual);
+            imprimirTaulell(taulell);
+
+            
+        }
+
+        scanner.close();  // Tancar l'scanner al final
+    }
+
+    // Inicialitza el taulell amb espais buits
+    public static void inicialitzarTaulell(char[][] taulell) {
+        for (int i = 0; i < taulell.length; i++) {
+            for (int j = 0; j < taulell[i].length; j++) {
+                taulell[i][j] = ' '; // Cambiar '-' por ' ' para un formato más limpio
             }
         }
-        
-        imprimirTablero(tablero);
-        
-
-
-        
-        
     }
-    
-    public static void imprimirTablero(char[][] tablero) {
-        System.out.print("  "); 
-        for (int j = 1; j <= tablero[0].length; j++) {
-            System.out.print(j + " "); 
-        }
+
+    // Mostra el taulell per pantalla amb el format desitjat
+    public static void imprimirTaulell(char[][] taulell) {
         System.out.println();
-        
-        for (int i = 1; i <= tablero.length; i++) {
-            System.out.print(i + " "); 
-            for (int j = 0; j < tablero[i-1].length; j++) {
-                System.out.print(tablero[i-1][j] + " ");
+
+        // Imprimir les files del taulell amb separadors
+        for (int i = 0; i < taulell.length; i++) {  // Iterar de 0 a files-1
+            System.out.print(" " + (taulell.length - i) + " | "); // Ajustar la numeración
+            for (int j = 0; j < taulell[i].length; j++) {
+                System.out.print(taulell[i][j] + " | ");
             }
             System.out.println();
+
+            // Línia separadora
+            System.out.print("   ");
+            for (int j = 0; j < taulell[i].length; j++) {
+                System.out.print("----");
+            }
+            System.out.println("-");
+        }
+
+        // Imprimir la numeració de les columnes
+        System.out.print("   ");
+        for (int j = 1; j <= taulell[0].length; j++) {
+            System.out.print("  " + j + " ");
+        }
+        System.out.println();
+    }
+
+    // Verifica si la columna seleccionada té espai
+    public static boolean columnaValida(char[][] taulell, int columna) {
+        if (columna < 0 || columna >= taulell[0].length) {
+            System.out.println("Columna fora de rang. Torna a intentar-ho.");
+            return false;
+        }
+
+        // Comprova si la columna està plena
+        if (taulell[0][columna] != ' ') {
+            System.out.println("Columna plena. Torna a intentar-ho.");
+            return false;
+        }
+
+        return true;
+    }
+
+    // Col·loca la fitxa a la columna seleccionada
+    public static void colocarFitxa(char[][] taulell, int columna, char jugador) {
+        for (int i = taulell.length - 1; i >= 0; i--) {
+            if (taulell[i][columna] == ' ') {
+                taulell[i][columna] = jugador;
+                break;
+            }
         }
     }
+    
 }
